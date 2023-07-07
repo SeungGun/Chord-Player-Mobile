c882aa336b316793720678e8574cef5900e63735
SeungGun <saverrw2864@naver.com>
2023-07-08 00:00:13 +0900

    style: 노래 목록 아이템의 장르 필드에 대한 PopMenu 추가

--- lib/SongItemWidget.dart
+++ lib/SongItemWidget.dart
@@ -15,117 +15,158 @@ class _SongItemWidgetState extends State<SongItemWidget> {
   @override
   Widget build(BuildContext context) {
     var size = MediaQuery.of(context).size;
-    return Container(
-      width: size.width * 0.98,
-      margin: EdgeInsets.all(size.width * 0.01),
-      padding: EdgeInsets.all(size.width * 0.02),
-      decoration: BoxDecoration(
-          color: Colors.grey[100],
-          border: Border.all(width: 0.5, color: Colors.black),
-          borderRadius: BorderRadius.circular(8)),
-      child: Column(
-        children: [
-          Row(
-            children: [
-              Row(
-                children: [
-                  Container(
-                    width: size.width * 0.82,
-                    alignment: Alignment.centerLeft,
-                    child: Text(
-                      widget.song.title,
-                      style:
-                          TextStyle(fontWeight: FontWeight.bold, fontSize: 17),
+    return GestureDetector(
+      onTap: () {
+        print('클릭 ${widget.song.songId}');
+      },
+      child: Container(
+        width: size.width * 0.98,
+        margin: EdgeInsets.all(size.width * 0.01),
+        padding: EdgeInsets.all(size.width * 0.02),
+        decoration: BoxDecoration(
+            color: Colors.grey[100],
+            border: Border.all(width: 0.5, color: Colors.black),
+            borderRadius: BorderRadius.circular(8)),
+        child: Column(
+          children: [
+            Row(
+              children: [
+                Row(
+                  children: [
+                    Container(
+                      width: size.width * 0.82,
+                      alignment: Alignment.centerLeft,
+                      child: Text(
+                        widget.song.title,
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold, fontSize: 17),
+                      ),
                     ),
-                  ),
-                ],
-              ),
-              Container(
-                  decoration: BoxDecoration(
-                      border: Border.all(width: 1),
-                      color: Colors.white,
-                      borderRadius: BorderRadius.circular(9)),
-                  child: IconButton(
-                      visualDensity: VisualDensity.compact,
-                      padding: EdgeInsets.all(0),
-                      onPressed: () {},
-                      icon: const Icon(
-                        Icons.note_alt_outlined,
-                        size: 30,
-                      )))
-            ],
-          ),
-          Row(
-            children: [
-              SizedBox(
-                width: size.width * 0.01,
-              ),
-              Text(
-                widget.song.artist,
-                style: TextStyle(color: Colors.grey, fontSize: 12),
-              ),
-            ],
-          ),
-          SizedBox(
-            height: size.height * 0.02,
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: [
-              Column(
-                children: [
-                  Text('원키'),
-                  SizedBox(height: size.height * 0.01),
-                  _valueBox(widget.song.originalKey, size,
-                      ChordUtil.getColorBySongKey(widget.song.originalKey)),
-                ],
-              ),
-              Column(
-                children: [
-                  Text('전조'),
-                  SizedBox(height: size.height * 0.01),
-                  _valueBox(widget.song.modulation, size,
-                      ChordUtil.getColorByModulation()),
-                ],
-              ),
-              Column(
-                children: [
-                  Text('BPM'),
-                  SizedBox(height: size.height * 0.01),
-                  _valueBox(widget.song.bpm.toString(), size,
-                      ChordUtil.getColorByBPM(widget.song.bpm!)),
-                ],
-              ),
-              Column(
-                children: [
-                  Text('성별'),
-                  SizedBox(height: size.height * 0.01),
-                  _valueBox(ChordUtil.convertGender(widget.song.gender), size,
-                      ChordUtil.getColorByGender(widget.song.gender)),
-                ],
-              ),
-              Column(
-                children: [
-                  Text('장르'),
-                  SizedBox(height: size.height * 0.01),
-                  Container(
-                    padding: EdgeInsets.all(size.width * 0.025),
+                  ],
+                ),
+                Container(
                     decoration: BoxDecoration(
-                        color: Colors.tealAccent,
                         border: Border.all(width: 1),
-                        borderRadius: BorderRadius.circular(7)),
-                    child: Text(
-                      widget.song.genres[0],
-                      style: TextStyle(color: Colors.black),
-                    ),
-                  )
-                  // _valueBox(widget.song.genres.toString(), size,
-                  //     ChordUtil.getColorByGender(widget.song.gender)),
-                ],
-              )
-            ],
-          ),
-        ],
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(9)),
+                    child: IconButton(
+                        visualDensity: VisualDensity.compact,
+                        padding: EdgeInsets.all(0),
+                        onPressed: () {
+                          showDialog(
+                              context: context,
+                              builder: (context) => AlertDialog(
+                                    title: Text('Note'),
+                                    content: Text(widget.song.note ?? ""),
+                                    actions: [
+                                      TextButton(
+                                          onPressed: () =>
+                                              Navigator.pop(context),
+                                          child: Text('닫기'))
+                                    ],
+                                  ));
+                        },
+                        icon: const Icon(
+                          Icons.event_note,
+                          size: 30,
+                        )))
+              ],
+            ),
+            Row(
+              children: [
+                SizedBox(
+                  width: size.width * 0.01,
+                ),
+                Text(
+                  widget.song.artist,
+                  style: TextStyle(color: Colors.grey, fontSize: 12),
+                ),
+              ],
+            ),
+            SizedBox(
+              height: size.height * 0.02,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                Column(
+                  children: [
+                    Text('원키', style: TextStyle(fontSize: 12)),
+                    SizedBox(height: size.height * 0.01),
+                    _valueBox(widget.song.originalKey, size,
+                        ChordUtil.getColorBySongKey(widget.song.originalKey)),
+                  ],
+                ),
+                Column(
+                  children: [
+                    Text('전조', style: TextStyle(fontSize: 12)),
+                    SizedBox(height: size.height * 0.01),
+                    _valueBox(widget.song.modulation, size,
+                        ChordUtil.getColorByModulation()),
+                  ],
+                ),
+                Column(
+                  children: [
+                    Text('BPM', style: TextStyle(fontSize: 12)),
+                    SizedBox(height: size.height * 0.01),
+                    _valueBox(widget.song.bpm.toString(), size,
+                        ChordUtil.getColorByBPM(widget.song.bpm!)),
+                  ],
+                ),
+                Column(
+                  children: [
+                    Text('성별', style: TextStyle(fontSize: 12)),
+                    SizedBox(height: size.height * 0.01),
+                    _valueBox(ChordUtil.convertGender(widget.song.gender), size,
+                        ChordUtil.getColorByGender(widget.song.gender)),
+                  ],
+                ),
+                Column(
+                  children: [
+                    Text('장르', style: TextStyle(fontSize: 12)),
+                    SizedBox(height: size.height * 0.01),
+                    PopupMenuButton<String>(
+                      itemBuilder: (BuildContext context) {
+                        return _convertGenresToPopupMenuItems(size);
+                      },
+                      shape: RoundedRectangleBorder(
+                        borderRadius:
+                            BorderRadius.circular(9), // 메뉴의 코너 둥글기 값 설정
+                      ),
+                      constraints: BoxConstraints(maxWidth: size.width * 0.8),
+                      padding: EdgeInsets.all(0),
+                      offset:
+                          Offset(size.width * 0.02 * -1, size.height * 0.03),
+                      child: Container(
+                        decoration: BoxDecoration(
+                          color: Colors.tealAccent,
+                          border: Border.all(width: 0.5),
+                          borderRadius: BorderRadius.circular(9),
+                        ),
+                        child: Padding(
+                          padding: EdgeInsets.all(size.width * 0.02),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Text(
+                                widget.song.genres[0],
+                                style: TextStyle(fontSize: 12),
+                              ),
+                              Icon(
+                                Icons.expand_more,
+                                size: 20,
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    )
+                  ],
+                )
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
@@ -141,5 +182,16 @@ class _SongItemWidgetState extends State<SongItemWidget> {
         style: TextStyle(color: pair['text']),
       ),
     );
+  }
+
+  List<PopupMenuEntry<String>> _convertGenresToPopupMenuItems(Size size) {
+    return widget.song.genres.map((String genre) {
+      return PopupMenuItem<String>(
+        value: genre,
+        height: 25,
+        padding: EdgeInsets.all(size.width * 0.01),
+        child: Text(genre, style: TextStyle(fontSize: 12)),
+      );
+    }).toList();
   }
 }
